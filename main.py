1963998a03d7e3a68ae51ecf35bc641735625474
DmitryLeonidovich <130282420+DmitryLeonidovich@users.noreply.github.com>
2023-05-28 01:27:02 +0300

    Start SB

---
+++ main.py
@@ -0,0 +1,28 @@
+# Exceptions
+class SBExceptions(Exception):
+    def __init__(self):
+        pass
+        
+        
+class BoardOutException(SBExceptions):
+    def __init__(self):
+        pass
+        
+        
+class DotAllReadyPoked(SBExceptions):
+    def __init__(self):
+                 DotTooClose,
+                 ):
+        self.BoardOutException = BoardOutException
+        self.DotAllReadyPoked = DotAllReadyPoked,
+        self.DotTooClose = DotTooClose
+        
+
+        
+
+
+
+# Frontend logics
+
+
+
