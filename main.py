34baaff849c95ac6e7c8163aa612fe5d1f1d0214
Dmitry <vdl99@list.ru>
2023-06-06 11:48:36 +0300

    добавлен код с началом UI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import winsound
 from itertools import chain
 from random import randint
+import time
 from typing import List
 
 
@@ -14,6 +15,7 @@ class SBException(Exception):
 
 class ShipNotFitted(SBException):
     def __init__(self, errmsg=''):
+        pew()
         self.errmsg = "Sea Battle exception:" + errmsg
         
         
@@ -42,22 +44,22 @@ class Dot:
         self.x = x
         self.y = y
     
-    def get_dot_xy(self):
+    def get_xy(self):
         return [self.x, self.y]
     
-    def get_dot_x(self):
+    def get_x(self):
         return self.x
     
-    def get_dot_y(self):
+    def get_y(self):
         return self.y
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
     
-    def dot_read(self, bat):
+    def read(self, bat):
         return bat[self.x][self.y]
     
-    def dot_wright(self, bat, val=0):
+    def wright(self, bat, val=0):
         bat[self.x][self.y] = val
         return
 
@@ -69,71 +71,81 @@ class Ship:
         self.size = size  # размер корабля
         self.dir_vec = dir_vec  # ориентация корабля 0- горизонтально слева на право 1- вертикально сверху вниз
         self.rest_of_cells = rest_of_cells  # остаток жизни корабля
-    
-    # @staticmethod
-    
-        
+         
     def dots(self):
-        sdl = direction(self.dir_vec)
-        dx = sdl[0]
-        dy = sdl[1]
-        sdl.clear()
+        sd_l = direction(self.dir_vec)
+        dx = sd_l[0]
+        dy = sd_l[1]
+        sd_l.clear()
         for i in range(self.size):
-            sdl.append(Dot(self.shp_x + i * dx, self.shp_y + i * dy))
-        return sdl
+            sd_l.append(Dot(self.shp_x + i * dx, self.shp_y + i * dy))
+        return sd_l
 
 
 class Board:
-    def __init__(self, dot_battle_field, battle_field, ships, visible, w_ship_rest, battle_field_size=6 ):
+    def __init__(self, dot_battle_field, battle_field, out_buf, ships, visible, w_ship_rest, battle_field_size=6 ):
         self.dot_battle_field = dot_battle_field
         self.battle_field = battle_field  # двумерный массив состояния игрового поля
+        self.out_buf = out_buf  # массив строк для вывода поля в консоль
         self.ships = ships  # список кораблей на доске
         self.visible = visible  # признак видимости кораблей при выводе содержимого поля
         self.w_ship_rest = w_ship_rest  # число оставшихся клеток живых кораблей
         self.battle_field_size = battle_field_size  # размерность игрового поля
-        
        
     def board_reset(self):  # сброс игрового поля
+        _out_buf = [
+            '    Игра с AI          ',
+            '   1 2 3 4 5 6 7 8 9 10',
+            ' 1 ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ',
+            ' 2 ◦ ░ ░ ░ ░ ░ ░ ◦ ◦ ▚ ',
+            ' 3 ◦ ░ ▚ ▚ ░ █ ░ ◦ X ◦ ',
+            ' 4 ◦ ░ ░ ░ ░ █ ░ ◦ ◦ ◦ ',
+            ' 5 ◦ ◦ ◦ ◦ ░ ▚ ░ ◦ ◦ ◦ ',
+            ' 6 ◦ ◦ ◦ ◦ ░ ░ ░ ◦ ◦ █ ',
+            ' 7 ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ █ ',
+            ' 8 █ █ ◦ █ ▚ █ ◦ ◦ ◦ ◦ ',
+            ' 9 ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ',
+            '10 ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ '
+        ]
         self.dot_battle_field: List[Dot] = []
         for x in range(self.battle_field_size):
             for y in range(self.battle_field_size):
                 self.dot_battle_field.append(Dot(x, y))
-        
-        print('Список Dot на поле:', self.dot_battle_field)
-        
         self.battle_field = [[0] * self.battle_field_size for _ in range(self.battle_field_size)]
-        self.ships = 0
+        self.out_buf = _out_buf
+        self.ships = []
         self.visible = True
         self.w_ship_rest = 0
         return
         
     def add_ship(self, sp_x, sp_y, s_dir, s_size):
+        
+        print('Запрос на корабль:', sp_x+1, sp_y+1, s_dir, s_size)
+        
         sdl = direction(s_dir)
         dx = sdl[0]
         dy = sdl[1]
         sdl.clear()
         for i in range(s_size):
             if self.battle_field[sp_y + i * dy][sp_x + i * dx] == 0:  # точка на поле свободна
-                sdl.append(Dot(sp_x + i * dx, sp_y + i * dy))  # в список точек корабля ее
-                self.battle_field[sp_y + i * dy][sp_x + i * dx] = 1  # метим точку кораблем
+                sdl.append(Dot(sp_x + i * dx, sp_y + i * dy))  # в список точек корабля
             else:
                 break
-        if len(sdl) == s_size:  # если корабль встал на поле, создадим контур корабля
-            sdc = Board.contour(self, sdl)
-            print('Контур корабля = ', end='')
-            for f in sdc:
-                print(Dot.get_dot_xy(f), end=' ')
-            print()
-            if len(sdc) > 0:
-                return sdl
-        if len(sdl) > 0:  # стираем корабль, не встает в выбранное место
-            for i in range(len(sdl)):
-                self.battle_field[sdl[i].y][sdl[i].x] = 0
-        raise ShipNotFitted(f'В точку [{sp_x + 1},{sp_y + 1}] установить корабль не удалось!')
+        if len(sdl) != s_size:  # если корабль НЕ встал на поле
+            raise ShipNotFitted(f'Из точки [{sp_x + 1},{sp_y + 1}] корабль расположить не получается!')
+        sdc = Board.contour(self, sdl)  # создадим контур корабля
+        for sc in sdc:  # проверим на близкие корабли по контуру
+            if self.battle_field[Dot.get_y(sc)][Dot.get_x(sc)] & 1:  # точка на контуре - другой корабль
+                raise ShipNotFitted(f'В точку [{sp_x + 1},{sp_y + 1}] установить корабль не удалось!')
+        if self.visible:
+            for sc in sdc:  # прорисуем контур корабля
+                sc_data = self.battle_field[Dot.get_y(sc)][Dot.get_x(sc)]
+                self.battle_field[Dot.get_y(sc)][Dot.get_x(sc)] = sc_data | 0b10
+        for sc in sdl:  # прорисуем корабль
+            self.battle_field[Dot.get_y(sc)][Dot.get_x(sc)] = 1
+        self.ships.append(sdl)
+        return sdl
     
-    # @staticmethod
-    
-        
     def contour(self, ship_cells):
         def dot_in_board(xc, yc):
             nonlocal d_c_l
@@ -142,101 +154,121 @@ class Board:
                 d_c_l.append(dt)
             return
         
+        size = len(ship_cells)
+        
         print("Координаты точек корабля = ", end='')
         for sc in ship_cells:
-            print(Dot.get_dot_xy(sc), sc in self.dot_battle_field, end=' ')
-        size = len(ship_cells)
+            print(Dot.get_xy(sc), '=', Dot.read(sc, self.battle_field), sc in self.dot_battle_field, end=' ')
         print('Size = ', size, end='\n')
         
-        cl_good = []
-        cl_dbl_good = []
-        
-        shp_dir = 0
+        shp_dir = 0  # False - горизонтально по умолчанию для одноклеточных кораблей
         if size > 1:
             sc = iter(ship_cells)  # установление по факту ориентации корабля для 2-х и более клеточных моделей
             shp_dir = get_dir(next(sc), next(sc))  # False - горизонтально True - вертикально
         d_c_l: Dot = []
         for sc in ship_cells:  # формирование списка точек контура вокруг корабля в рамках игрового поля
-            x = Dot.get_dot_x(sc)
-            y = Dot.get_dot_y(sc)
+            x = Dot.get_x(sc)
+            y = Dot.get_y(sc)
             if shp_dir == 0:  # для горизонтали
                 dot_in_board(x - 1, y)
                 dot_in_board(x + size, y)
                 for dx in range(-1, size + 1):
                     dot_in_board(x + dx, y + 1)
                     dot_in_board(x + dx, y - 1)
-            else:
+            else:  # для вертикали
                 dot_in_board(y, x - 1)
                 dot_in_board(y, x + size)
                 for dx in range(-1, size + 1):
                     dot_in_board(y + 1, x + dx)
                     dot_in_board(y - 1, x + dx)
-            """
-            print('         d_c_l = ', end='')
-            for f in d_c_l:
-                print(Dot.get_dot_xy(f), end=', ')
-            print()
-            """
-            
-            cont_dump = []
-            for i in range(len(cl_good)):  # сохраняем контур на случай отката
-                cont_dump.append(self.battle_field[cl_good[i][1]][cl_good[i][0]])
-                # print(cont_dump, cl_good[i][1], cl_good[i][0])
-            for i in range(len(cl_good)):  # метим контур
-                ship_too_close = self.battle_field[cl_good[i][1]][cl_good[i][0]] == 1
-                if ship_too_close:  # признак нахождения рядом с соседним кораблем
-                    for j in range(len(cl_good)):  # восстанавливаем контур на поле боя
-                        self.battle_field[cl_good[j][1]][cl_good[i][0]] = cont_dump[j]
-                    cl_good.clear()  # стираем список точек контура
-                    break
-                else:
-                    self.battle_field[cl_good[i][1]][cl_good[i][0]] = 2
-            break
+            break  # достаточно одного прохода
         return d_c_l
     
-    def out_bf_raw(self):
-        print('\u00A6' + '=' * self.battle_field_size * 3 + '\u21d2 X')
+    def out_raw(self):
+        print(chr(0x00A6) + '=' * self.battle_field_size * 3 + chr(0x21d2) + ' X')
         for b in range(self.battle_field_size):
-            print('\u00A6' + str(self.battle_field[b]))
-        print('\u21d3' + '-' * (self.battle_field_size * 3 -1))
+            print(chr(0x00A6) + str(self.battle_field[b]))
+        print(chr(0x21d3) + '-' * (self.battle_field_size * 3 -1))
         print('Y')
         return
     
-    def screen_update(self, position='L'):
-        s = ' ' + ' 0 1 2'
-        print(s)
-        s = ' ' + chr(0x22f1) + '0 1 2 \u25a0'
-        print(s)
-        # for x in range(self.battle_field_size):
-        #     for y in range(self.battle_field_size):
-        #         pass
-        i = 0
-        # print("Character set")
-        # start = 8500
-        # for x in range(0xFF):
-        #     for y in range(25):
-        #         #s = '\' + 'u' + f'{i+9632:04X}'
-        #         s = hex(i+start)
-        #         s = s[2:]
-        #         s = chr(92) + 'u' + s
-        #         print(f'{i+start:04X}=' + chr(i + start), end=' ')
-        #         i += 1
-        #     print()
-        # print('\u274c')
-        # print('\u274e')
-        # print('\u2b1b')
-        # print('\u2b1c')
-        # print('\u26d5')
-        # print('\u2591')
-        # print('\u2588')
-        # print('\u229a')
-        
-        return
-  
     def dot_is_free(self, xf, yf):
         return self.battle_field[xf][yf] == 0
+    
+    
+free_cell_sgn = '◦'  # свободная клетка
+ship_sgn = '█'  # в клетке корабль (или его часть)
+con_ship_sgn = '░'  # контур вокруг корабля
+dead_ship_sgn = '▚'  # в клетке подбитый корабль (или его часть)
+miss_fire_sgn = 'X'  # в клетку стреляли, но промахнулись
 
 
+def bat_fld_analizer(bt_in):
+    for y in range(bt_in.battle_field_size):
+        for x in range(bt_in.battle_field_size):
+            sig = free_cell_sgn
+            cell_data = bt_in.battle_field[y][x]
+            if cell_data == 1:  # клетка с кораблем
+                if bt_in.visible:
+                    sig = ship_sgn
+                else:
+                    sig = free_cell_sgn
+                    if cell_data & 4 == 4:  # корабль подбит, показываем
+                        sig = dead_ship_sgn
+            else:
+                if cell_data == 2:  # клетка контур
+                    sig = con_ship_sgn
+                    if cell_data & 4 == 4:  # в клетку был выстрел
+                        sig = miss_fire_sgn
+            s1 = str(bt_in.out_buf[y + 2])
+            #s1 = ' 1 abcdefjhigklmnopqrst'
+            s2 = s1
+            x_ind = 3 + x * 2
+            s1 = s1[:x_ind]
+            s2 = s2[x_ind + 1:]
+            s0 = s1 + sig + s2
+            bt_in.out_buf[y + 2] = s0
+    return
+
+def screen_update(position='L'):
+    def l_r_out():  # вывод левого и правого игровых полей
+        max_battle_field_size = bf.battle_field_size
+        if uf.battle_field_size > max_battle_field_size:
+            max_battle_field_size = uf.battle_field_size
+        for i in range(max_battle_field_size + 2):
+            s_l = str(bf.out_buf[i])
+            s_r = str(uf.out_buf[i])
+            s_lr = s_l[:(3 + 2 * bf.battle_field_size)] + '          ' + s_r[:(3 + 2 * uf.battle_field_size)]
+            print(s_lr)
+        print()
+        return
+    
+    def l_out():  # вывод левого игрового поля
+        for i in range(bf.battle_field_size + 2):
+            s_l = str(bf.out_buf[i])
+            s_lr = s_l[:(3 + 2 * bf.battle_field_size)]
+            print(s_lr)
+        print()
+        return
+    
+    def r_out():  # вывод правого игрового поля
+        for i in range(bf.battle_field_size + 2):
+            s_r = str(uf.out_buf[i])
+            s_lr = s_r[:(3 + 2 * uf.battle_field_size)]
+            print(s_lr)
+        print()
+        return
+    
+    if position == 'L':
+        l_out()
+    elif position == 'R':
+        r_out()
+    elif position == 'LR':
+        l_r_out()
+    else:
+        bf.out_raw()
+    return
+    
 # unclassified functions
 def direction(s_dir=0):
     if s_dir == 0:
@@ -249,7 +281,7 @@ def direction(s_dir=0):
     
     
 def get_dir(d1, d2):
-    return Dot.get_dot_x(d1) == Dot.get_dot_x(d2)
+    return Dot.get_x(d1) == Dot.get_x(d2)
 
 
 # Internal logic
@@ -261,10 +293,16 @@ def pew(snd='pew.wav'):
     winsound.PlaySound(snd, winsound.SND_FILENAME)
     return
 
-bf = Board(dot_battle_field=None, battle_field=None, ships=None, visible=None, w_ship_rest=None, battle_field_size=10)
+bf = Board(dot_battle_field=None, battle_field=None, out_buf=None,\
+           ships=None, visible=None, w_ship_rest=None, battle_field_size=10)
 bf.board_reset()
+bf.visible = True
 
-bf. out_bf_raw()
+uf = Board(dot_battle_field=None, battle_field=None, out_buf=None,\
+           ships=None, visible=None, w_ship_rest=None, battle_field_size=10)
+uf.board_reset()
+
+bf.out_raw()
 
 try:
     sh = bf.add_ship(1, 1, 1, 2)
@@ -272,15 +310,15 @@ except ShipNotFitted as er:
     print(er)
 else:
     pass
-bf. out_bf_raw()
+bf.out_raw()
 
 try:
-    sh = bf.add_ship(3, 0, 0, 2)
+    sh = bf.add_ship(3, 0, 0, 3)
 except ShipNotFitted as er:
     print(er)
 else:
     pass
-bf. out_bf_raw()
+bf.out_raw()
 
 try:
     sh = bf.add_ship(2, 3, 0, 3)
@@ -288,16 +326,39 @@ except ShipNotFitted as er:
     print(er)
 else:
     pass
-bf. out_bf_raw()
 
-bf.screen_update()
+Dot.wright(Dot(4,4), bf.battle_field, 4)
+Dot.wright(Dot(4,4), bf.battle_field, 6)
+Dot.wright(Dot(5,5), bf.battle_field, 4)
+Dot.wright(Dot(6,6), bf.battle_field, 2)
+Dot.wright(Dot(7,7), bf.battle_field, 1)
+Dot.wright(Dot(8,8), bf.battle_field, 8)
+Dot.wright(Dot(9,9), bf.battle_field, 9)
 
-"""
-    for x in range(6):
-        for y in range(6):
-            sdl.append(Dot(x, y))  # в список точек
-    
-    for sc in sdl:
-        print(Dot.dot_read(sc, bf.battle_field), ' = ', end='')
-        print(Dot.get_dot_xy(sc), Dot.get_dot_x(sc), Dot.get_dot_y(sc))
-"""
+
+bf.out_raw()
+
+
+bat_fld_analizer(bf)
+screen_update('LR')
+
+Dot.wright(Dot(1,1), bf.battle_field, 1)
+ps = 0
+while ps < 10:
+    print('*', end='')
+    time.sleep(1)
+    ps += 1
+print()
+
+bat_fld_analizer(bf)
+screen_update('LR')
+
+Dot.wright(Dot(1,2), bf.battle_field, 1)
+ps = -1
+while ps < 10:
+    print('*', end='')
+    time.sleep(1)
+    ps += 1
+print()
+bat_fld_analizer(bf)
+screen_update('LR')
