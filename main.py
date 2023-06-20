ceb5bf3c8102ff3f37a3322a48be6c270b74dd14
Dmitry <vdl99@list.ru>
2023-06-20 21:01:00 +0300

    добавлен код с началом UI !!!!!!!!

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import self as self
 import winsound
-from itertools import chain
 from random import randint
 import time
 from typing import List
+
+
+FREE_CELL_SGN = '◦'  # свободная клетка
+SHIP_SGN = '█'  # в клетке корабль (или его часть)
+CON_SHIP_SGN = '░'  # контур вокруг корабля
+DEAD_SHIP_SGN = '▚'  # в клетке подбитый корабль (или его часть)
+MISS_FIRE_SGN = 'X'  # в клетку стреляли, но промахнулись
+S_DIR_STR = ['горизонтально', 'вертикально']  # строка индикации направления корабля
+SHIP = 0b0001  # флаг корабля в клетке
+CONT = 0b0010  # флаг корабельного контура
+FIRE = 0b0100  # флаг выстрела в клетку
+C_DS = 0b1000  # флаг контура подбитого корабля
 
 
 # Exceptions block
@@ -92,14 +104,15 @@ class Board:
         self.ships = ships  # список кораблей на доске
         self.visible = visible  # признак видимости кораблей при выводе содержимого поля
         self.w_ship_rest = w_ship_rest  # число оставшихся клеток живых кораблей
-        self.battle_title = battle_title # заголовок окна "Поле "______
+        self.battle_title = battle_title  # заголовок окна "Поле "______
         self.battle_field_size = battle_field_size  # размерность игрового поля
        
     def board_reset(self):  # сброс игрового поля
         _out_buf = [
-            #00000000001111111111222
-            #01234567890123456789012
-            '      Поле             ',
+            # 0000000001111111111222
+            # 1234567890123456789012
+            
+            ' Поле                  ',
             '   1 2 3 4 5 6 7 8 9 10',
             ' 1 ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ◦ ',
             ' 2 ◦ ░ ░ ░ ░ ░ ░ ◦ ◦ ▚ ',
@@ -125,14 +138,14 @@ class Board:
             if len(s) > 6:
                 s = s[:6]
             ls = len(_out_buf[0])
-            _out_buf[0] = _out_buf[0][:11] + s
+            _out_buf[0] = _out_buf[0][:6] + s
             while len(_out_buf[0]) < ls:
                 _out_buf[0] = _out_buf[0] + ' '
         return
         
     def add_ship(self, sp_x, sp_y, s_dir, s_size):
         print('Поле', self.battle_title, end=' ')
-        print(f'Запрос на корабль: X={sp_x+1:2d} Y={sp_y+1:2d}', S_DIR_STR[s_dir], "размер=",s_size)
+        print(f'Запрос на корабль: X={sp_x+1:2d} Y={sp_y+1:2d}', S_DIR_STR[s_dir], "размер=", s_size)
         dx = direction(s_dir)[0]
         dy = direction(s_dir)[1]
         sdl = []
@@ -150,9 +163,9 @@ class Board:
                 raise ShipNotFitted(f'В точку [{sp_x + 1},{sp_y + 1}] установить корабль не удалось!')
         if self.visible:
             for sc in sdc:  # прорисуем контур корабля
-                sc.status = sc.status | 0b10
+                sc.status = sc.status | CONT
         for sc in sdl:  # прорисуем корабль
-            sc.status = 1
+            sc.status = SHIP
         self.ships.append(sdl)  # внесем корабль в группу кораблей поля боя
         return sdl  # вернем список точек корабля при успехе
     
@@ -209,7 +222,6 @@ class Board:
         for b in range(self.battle_field_size):
             print(chr(0x00A6), end=' ')
             for a in range(self.battle_field_size):
-                #ds = a + b * self.battle_field_size
                 d = self.battle_field[a + b * self.battle_field_size]
                 print(d.status, end=' ')
             print()
@@ -226,12 +238,40 @@ class Board:
             raise DotAllReadyPoked('Сюда уже стреляли!')
         if self.dot_with_flags(x, y, SHIP):  # попали в корабль
             dt.status = dt.status | SHIP  # отмечаем, что в клетке корабль
+            return True
         dt.status = dt.status | FIRE  # отмечаем, что в клетку произведен выстрел
-        return
+        return False
     
     def dot_with_flags(self, xf, yf, flag):  # возвращает True если флаги по маске установлены
         dt = self.battle_field[xf + yf * self.battle_field_size]  # считываем из поля боя клетку
         return dt.status & flag == flag
+    
+    def bat_fld_analyzer(self):
+        for y in range(self.battle_field_size):
+            for x in range(self.battle_field_size):
+                sig = FREE_CELL_SGN  # по умолчанию - свободное поле
+                bf_pos = x + y * self.battle_field_size
+                cell = self.battle_field[bf_pos]
+                #print(self.battle_field)
+                if cell.status & 1 == 1:  # клетка с кораблем
+                    if cell.status & 4 == 4:  # корабль подбит
+                        sig = DEAD_SHIP_SGN  # показываем подбитый
+                    elif self.visible:
+                        sig = SHIP_SGN  # показываем целый
+                else:
+                    if cell.status & 4 == 4:  # в клетку был выстрел
+                        sig = MISS_FIRE_SGN  # показываем выстрел
+                    elif cell.status & 8 == 8:  # клетка - контур подбитого корабля
+                        sig = CON_SHIP_SGN  # показываем контур
+                s1 = str(self.out_buf[y + 2])
+                s2 = s1
+                x_ind = 3 + x * 2
+                s1 = s1[:x_ind]
+                s2 = s2[x_ind + 1:]
+                s0 = s1 + sig + s2
+                self.out_buf[y + 2] = s0
+        return
+    
     
 class Player:
     def __init__(self, my_board, enemy_board):
@@ -241,94 +281,49 @@ class Player:
     def ask(self):
         raise NotImplementedError()
     
-    # def move(self):
-    #     while True:
-    #         try:
-    #             xy_coord = self.ask()
-            
-    
-    
-    
-
+    def move(self):
+        while True:
+            try:
+                xy_coord = self.ask()
+                hit_ok = self.enemy_board.shot(self, xy_coord.x, xy_coord.y)
+            except BoardOutException as e:
+                print(e)
+                continue
+            if hit_ok:
+                continue
+        return
         
-    
-    
-FREE_CELL_SGN = '◦'  # свободная клетка
-SHIP_SGN = '█'  # в клетке корабль (или его часть)
-CON_SHIP_SGN = '░'  # контур вокруг корабля
-DEAD_SHIP_SGN = '▚'  # в клетке подбитый корабль (или его часть)
-MISS_FIRE_SGN = 'X'  # в клетку стреляли, но промахнулись
-S_DIR_STR = ['горизонтально', 'вертикально']  # строка индикации направления корабля
-SHIP = 0b0001  # флаг корабля в клетке
-CONT = 0b0010  # флаг корабельного контура
-FIRE = 0b0100  # флаг выстрела в клетку
-CNDS = 0b1000  # флаг контура подбитого корабля
+        
+class AI(Player):
+    def ask(self, ):
+        dum = []
+        for i in self.enemy_board.battle_field:  # создание списка точек для удара на поле игрока
+            if i.status & (FIRE + C_DS) == 0:
+                dum.append(i)
+        if len(dum) < 1:
+            raise BoardOutException("Нет места для выстрела!!!")
+        d_ind = randint(0, len(dum))  # выбор свободной точки для выстрела в поле врага
+        d = dum[d_ind]
+        print(f"Удар в точку: {d.x + 1} {d.y + 1}")
+        return d
 
-def bat_fld_analyzer(bt_in):
-    for y in range(bt_in.battle_field_size):
-        for x in range(bt_in.battle_field_size):
-            sig = FREE_CELL_SGN  # по умолчанию - свободное поле
-            cell = bt_in.battle_field[x + y * bt_in.battle_field_size]
-            if cell.status & 1 == 1:  # клетка с кораблем
-                if cell.status & 4 == 4:  # корабль подбит
-                    sig = DEAD_SHIP_SGN  # показываем подбитый
-                elif bt_in.visible:
-                    sig = SHIP_SGN  # показываем целый
-            else:
-                if cell.status & 4 == 4:  # в клетку был выстрел
-                    sig = MISS_FIRE_SGN  # показываем выстрел
-                elif cell.status & 8 == 8:  # клетка - контур подбитого корабля
-                    sig = CON_SHIP_SGN  # показываем контур
-            s1 = str(bt_in.out_buf[y + 2])
-            s2 = s1
-            x_ind = 3 + x * 2
-            s1 = s1[:x_ind]
-            s2 = s2[x_ind + 1:]
-            s0 = s1 + sig + s2
-            bt_in.out_buf[y + 2] = s0
-    return
 
-def screen_update(position='L'):
-    def l_r_out():  # вывод левого и правого игровых полей
-        max_battle_field_size = bf.battle_field_size
-        if uf.battle_field_size > max_battle_field_size:
-            max_battle_field_size = uf.battle_field_size
-        for i in range(max_battle_field_size + 2):
-            s_l = str(bf.out_buf[i])
-            s_r = str(uf.out_buf[i])
-            s_lr = s_l[:(3 + 2 * bf.battle_field_size)] + '          ' + s_r[:(3 + 2 * uf.battle_field_size)]
-            print(s_lr)
-        print()
-        return
+class User(Player):
+    def ask(self):
+        while True:
+            xy = input("Введите X и Y через пробел: ").split()
+            if len(xy) != 2:
+                print("Введите только 2 координаты!")
+                continue
+            x, y = xy
+            if not (x.isdigit()) or not (y.isdigit()):
+                print(" Вводите числа! ")
+                continue
+            x, y = int(x), int(y)
+            return Dot(x - 1, y - 1)
+
     
-    def l_out():  # вывод левого игрового поля
-        for i in range(bf.battle_field_size + 2):
-            s_l = str(bf.out_buf[i])
-            s_lr = s_l[:(3 + 2 * bf.battle_field_size)]
-            print(s_lr)
-        print()
-        return
-    
-    def r_out():  # вывод правого игрового поля
-        for i in range(uf.battle_field_size + 2):
-            s_r = str(uf.out_buf[i])
-            s_lr = s_r[:(3 + 2 * uf.battle_field_size)]
-            print(s_lr)
-        print()
-        return
-    
-    if position == 'L':
-        l_out()
-    elif position == 'R':
-        r_out()
-    elif position == 'LR':
-        l_r_out()
-    else:
-        Board.out_raw(bf)
-        Board.out_raw(uf)
-    return
-    
-    
+
 # unclassified functions
 def direction(s_dir=0):
     if s_dir == 0:
@@ -364,78 +359,114 @@ def pause(t):
     return
 
 
-bf = Board(battle_field=None, out_buf=None,
-           ships=None, visible=None, w_ship_rest=None, battle_title='AI bot', battle_field_size=10)
-bf.board_reset()
-bf.visible = True
 
-uf = Board(battle_field=None, out_buf=None,
-           ships=None, visible=None, w_ship_rest=None, battle_title='User', battle_field_size=10)
-uf.board_reset()
-uf.visible = True
+class Game:
+    def __init__(self):
+        self.bf = Board(battle_field=None, out_buf=None,
+                        ships=None, visible=True, w_ship_rest=None, battle_title='AI bot', battle_field_size=10)
+        self.uf = Board(battle_field=None, out_buf=None,
+                        ships=None, visible=True, w_ship_rest=None, battle_title='User', battle_field_size=10)
+        self.guru = AI(self.bf.battle_field, self.uf.battle_field)
+        self.nemo = User(self.uf.battle_field, self.bf.battle_field)
+        self.random_board(self.uf)
+        self.random_board(self.bf)
 
-bf.out_raw()
-
-try:
-    sh = bf.add_ship(1, 1, 1, 2)
-except ShipNotFitted as er:
-    print(er)
-else:
-    pass
-bf.out_raw()
-
-try:
-    sh = bf.add_ship(3, 0, 0, 3)
-except ShipNotFitted as er:
-    print(er)
-else:
-    pass
-bf.out_raw()
-
-try:
-    sh = bf.add_ship(2, 3, 0, 3)
-except ShipNotFitted as er:
-    print(er)
-else:
-    pass
-
-bat_fld_analyzer(bf)
-bf.out_raw()
-
-screen_update('L')
-screen_update('R')
-
-screen_update('LR')
-
-drx = 1
-dry = 1
-dd = bf.battle_field[drx + dry * bf.battle_field_size]
-dr = dd.status
-dr = dr | 0b100
-dd.status = dr
-pause(3)
-bat_fld_analyzer(bf)
-bf.out_raw()
-screen_update('LR')
-
-drx = 1
-dry = 2
-dr = bf.battle_field[drx + dry * bf.battle_field_size].status
-dr = dr | 0b100
-bf.battle_field[drx + dry * bf.battle_field_size].status = dr
-pause(3)
-bat_fld_analyzer(bf)
-bf.out_raw()
-screen_update('LR')
-
-drx = 2
-dry = 2
-dr = bf.battle_field[drx + dry * bf.battle_field_size].status
-dr = dr | 0b100
-bf.battle_field[drx + dry * bf.battle_field_size].status = dr
-pause(3)
-bat_fld_analyzer(bf)
-bf.out_raw()
-screen_update('LR')
-screen_update('ub')
-
+    def random_board(self, bd):
+        while True:
+            bd.board_reset()
+            ship_type = [3, 2, 2, 1, 1, 1, 1]
+            att1 = True
+            while att1:
+                steps = 0
+                for st in ship_type:
+                    while True:
+                        steps += 1
+                        try:
+                            #bd.add_ship(randint(0, bd.battle.size), randint(0, bd.battle.size), randint(0, 1), st)
+                            break  # корабль встал удачно, берем следующий, если есть
+                        except ShipNotFitted:
+                            pass  # корабль не вписался в поле
+                        if steps > 2000:  # смотрим, есть ли еще лимит попыток размещения
+                            att1 = False  # лимит попыток исчерпан - сброс игрового поля как тупикового
+                            break
+                    if not att1:
+                        break  # Выходим из цикла по размещению кораблей, т.к. нужно сбросить поле
+                if att1:
+                    return  # если цикл по кораблям закончился традиционно - выходим, все они установлены
+                pass  # нет, цикл еще с перспективой - берем следующий корабль на размещение
+            pass  # сюда попадают при необходимости сбросить поле
+        pass  # отсюда сброс поля и делают
+        # сюда никогда не попадают
+    
+    
+    def screen_update(self, bf, uf, position='L'):
+        def l_r_out():  # вывод левого и правого игровых полей
+            max_battle_field_size = bf.battle_field_size
+            if uf.battle_field_size > max_battle_field_size:
+                max_battle_field_size = uf.battle_field_size
+            for i in range(max_battle_field_size + 2):
+                if i > bf.battle_field_size + 1:
+                    s_l = " " * (3 + 2 * bf.battle_field_size)
+                else:
+                    s_l = str(bf.out_buf[i])
+                if i > uf.battle_field_size + 1:
+                    s_r = " " * (3 + 2 * uf.battle_field_size)
+                else:
+                    s_r = str(uf.out_buf[i])
+                s_lr = s_l[:(3 + 2 * bf.battle_field_size)] + '          ' + s_r[:(3 + 2 * uf.battle_field_size)]
+                print(s_lr)
+            print()
+            return
+        
+        def l_out():  # вывод левого игрового поля
+            for i in range(bf.battle_field_size + 2):
+                s_l = str(bf.out_buf[i])
+                s_lr = s_l[:(3 + 2 * bf.battle_field_size)]
+                print(s_lr)
+            print()
+            return
+        
+        def r_out():  # вывод правого игрового поля
+            for i in range(uf.battle_field_size + 2):
+                s_r = str(uf.out_buf[i])
+                s_lr = s_r[:(3 + 2 * uf.battle_field_size)]
+                print(s_lr)
+            print()
+            return
+        
+        if position == 'L':
+            l_out()
+        elif position == 'R':
+            r_out()
+        elif position == 'LR':
+            l_r_out()
+        else:
+            Board.out_raw(bf)
+            Board.out_raw(uf)
+        return
+    
+    
+    
+    def greetings(self):
+        print("Hello!!! This is Sea Battle.")
+        
+    def loop(self):
+        curr_move = 0  # ноль - игрок, 1 - компьютер
+        while True and curr_move < 1:
+            self.bf.bat_fld_analyzer()
+            self.uf.bat_fld_analyzer()
+            self.screen_update(self.bf, self.uf, "LR")
+            pause(3)
+            curr_move += 1
+        return
+    
+    def start(self):
+        self.greetings()
+        self.loop()
+      
+        
+game = Game()
+game.start()
+game.screen_update(game.bf, game.uf, "L")
+print("Доброго вечера, мы со Skill Factory.")
+quit(0)
